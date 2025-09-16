81ed5c27985d99d2a1a942a3108ced5ddbc59cbc
Monnyka <31350922+Monnyka@users.noreply.github.com>
2025-09-16 22:24:04 +0700

    Update and resolve conflict file

--- routes/task.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getAllItems, createTask,getTask, deleteTask, updateTask } = require("../controllers/task");
-
-router.route('/').get(getAllItems).post(createTask)
-router.route('/:id').get(getTask).delete(deleteTask).patch(updateTask)
-
-module.exports = router;
